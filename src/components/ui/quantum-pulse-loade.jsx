97a7d048433d835f351d7e1c08967da1e67cd105
toqaelsayed <tokaelsayed31@gmail.com>
2026-01-07 16:31:29 +0200

    Add quantum-pulse-loade.jsxDetails:- Small loader animation (UI atom)- Can be used inside SplashLoader or anywhere a small loader is needed- Lightweight and reusable

---
+++ src/components/ui/quantum-pulse-loade.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const QuantumPulseLoader = () => {
+  return (
+   <div className="flex justify-center items-center gap-3 h-16">
+  <span className="w-3 h-3 bg-blue-500 rounded-full animate-bounce-delay1"></span>
+  <span className="w-3 h-3 bg-blue-500 rounded-full animate-bounce-delay2"></span>
+  <span className="w-3 h-3 bg-blue-500 rounded-full animate-bounce-delay3"></span>
+</div>
+
+  );
+};
+
+export default QuantumPulseLoader;
